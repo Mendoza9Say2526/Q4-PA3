ac536459263fed99e67f933dabba624e9e0db21f
Mendoza9Say2526 <mendoza.jm@obmontessori.edu.ph>
2026-04-20 11:06:01 +0800

    better library stuff

---
+++ main.js
@@ -0,0 +1,18 @@
+﻿function displaySection() {
+    var sectionCode =
+        document.getElementById("sectionInput").value;
+
+    switch (sectionCode) {
+        case "F":
+            document.getElementById('result').innerHTML = "You're Checking the Fiction Section!";
+            break;
+        case "N":
+            document.getElementById('result').innerHTML = "You're Checking the Non-Fiction Section!";
+            break;
+        case "R":
+            document.getElementById('result').innerHTML = "You're Checking the Reference Section!";
+            break;
+        default:
+            document.getElementById('result').innerHTML = "Invalid section code. Please enter F, N, or R.";
+    }
+}
